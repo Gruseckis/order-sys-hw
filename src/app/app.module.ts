2c9f7f3da099019230b9016d784f93ab1af19198
Asus <Asus@MacBook-Air.local>
2020-01-10 15:17:47 +0200

    Initial setup commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      NavBarComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MatButtonModule,
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
