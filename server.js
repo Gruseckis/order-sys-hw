7b220db668570c783f030e2154702ce16295a93f
Germans Gruseckis <g.gruseckis@googlemail.com>
2020-01-16 20:12:14 +0200

    Add json server to build

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db/data.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use(router)
+server.listen(3001, () => {
+  console.log('JSON Server is running')
+})
+
 app.use(express.static(__dirname + '/dist/order-system-pf'));
 
 app.get('/*', function(req, res) {
