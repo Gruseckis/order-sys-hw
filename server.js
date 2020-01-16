c484cef81f0ae76559501ce73c825db8bf05ba2d
Germans Gruseckis <g.gruseckis@googlemail.com>
2020-01-16 20:25:44 +0200

    add server config

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db/data.json')
@@ -12,11 +8,3 @@ server.use(router)
 server.listen(3001, () => {
   console.log('JSON Server is running')
 })
-
-app.use(express.static(__dirname + '/dist/order-system-pf'));
-
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/order-system-pf/index.html'));
-});
-
-app.listen(process.env.PORT || 3000);
